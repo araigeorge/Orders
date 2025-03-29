425f4400ae99236c2a4353ad09f8356f09d8481b
araigeorge <araigeorge@gmail.com>
2025-03-28 23:13:48 -0400

    Create Database with the first entity country

---
+++ Orders/Orders.Backend/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Orders.Shared.Entities;
+using System.Data.Common;
+
+namespace Orders.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+        
+    }
+}
